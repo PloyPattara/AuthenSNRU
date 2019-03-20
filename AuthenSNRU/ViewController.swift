f5233976e97e4c398555700d5fa7133ac4fa7a83
PloyNoii <pattharawadee.pe59@snru.ac.th>
2019-03-20 14:17:16 +0700

    Setup Seague Success

--- AuthenSNRU/ViewController.swift
+++ AuthenSNRU/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     @IBAction func registerButton(_ sender: UIButton) {
         
         print("You Click Register")
+        
+        performSegue(withIdentifier: "GoRegister", sender: self)
     }
     
     
